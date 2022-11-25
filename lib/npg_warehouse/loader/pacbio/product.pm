89837e02879a466cd6f93b4e2b09d415649d8a51
mksanger <mk35@sanger.ac.uk>
2022-11-25 15:51:12 +0000

    Move tag parsing into separate function

--- lib/npg_warehouse/loader/pacbio/product.pm
+++ lib/npg_warehouse/loader/pacbio/product.pm
@@ -53,11 +53,7 @@ sub product_data {
       # samples from the same well with different tags and deplexed reads can
       # be differentiated
       while (my $row = $pac_bio_run->next) {
-        my $tags = $row->tag_sequence;
-        my $tag2 = $row->tag2_sequence;
-        if ($tag2){
-          $tags .= "$TAG_DELIM$tag2";
-        }
+        my $tags = get_tags($row);
 
         my $id_product = $self->generate_product_id(
           $well->{'pac_bio_run_name'}, $well->{'well_label'}, $tags);
@@ -71,6 +67,31 @@ sub product_data {
     }
   }
   return \@product_data;
+}
+
+=head2 get_tags
+
+  Arg [1]    : Row from pac_bio_run table, DBIx Result. Required.
+  Example    : $tags = get_tags($row);
+  Description: Checks whether there are tag sequences in the row, and
+               returns them as a comma separated list which can be used
+               as part of product id generation
+  Returntype : String
+
+=cut
+
+sub get_tags {
+  my ($row) = @_;
+
+  my $tag1 = $row->tag_sequence;
+  my $tag2 = $row->tag2_sequence;
+  if ($tag1){
+    if ($tag2) {
+      return join $TAG_DELIM, $tag1, $tag2;
+    }
+    return $tag1;
+  }
+  return q[];
 }
 
 =head2 load_pacbioproductmetric_table
